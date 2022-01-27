26ea9d38de2197a69fe3044e4f3e4288313bd47e
chankruze <chankruze@gmail.com>
2022-01-28 03:41:22 +0530

    docs: update travis config & deployment script

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -26,6 +26,6 @@ exec('git config user.name "chankruze"')
 exec('git config user.email "chankruze@gmail.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
-  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
+  `git push --force --quiet "https://${ghToken}@${repository}" main:gh-pages`
 )
 echo("Docs deployed!!")
