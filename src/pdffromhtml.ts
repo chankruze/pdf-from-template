a4a68ee67b0079219e6586dfeb7a21c7cf06ef06
chankruze <chankruze@gmail.com>
2022-01-28 03:25:29 +0530

    finish minimal working version
    * update test cases and linting rule
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/pdffromhtml.ts
+++ src/pdffromhtml.ts
@@ -8,14 +8,51 @@ Copyright (c) geekofia 2022 and beyond
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 
-const PdfFromHtml = (content: string, fileName: string) => {
-    return new Promise((resolve, reject) => {
-        try {
-            resolve(Buffer.from("OK"));
-        } catch (err) {
-            reject(err);
-        }
-    });
-};
+const puppeteer = require("puppeteer");
+const inlineCss = require("inline-css");
+const handlebars = require("handlebars");
+const path = require("path");
+const fs = require("fs");
 
-export default PdfFromHtml;
+export async function pdfFromHtml(
+    content: string,
+    fileName: string,
+    outDir: string,
+): Promise<Buffer> {
+    // create a browser instance
+    const browser = await puppeteer.launch();
+    // new page
+    const page = await browser.newPage();
+
+    // TODO: validate HTML
+
+    // convert external css to inline (inline-css)
+    const formattedHTML = await inlineCss(content, { url: "/" });
+
+    // compile the template with handlebars
+    // TODO: insert values to variables in the template
+    // (https://handlebarsjs.com/guide/#installation)
+    const template = handlebars.compile(formattedHTML, { strict: true })();
+
+    // set page content and wait for it to load completely
+    await page.setContent(template, {
+        waitUntil: "networkidle0",
+    });
+
+    // TODO: check if path exists
+    if (!fs.existsSync(path.resolve(outDir, fileName))) {
+        fs.mkdirSync(path.resolve(outDir), { recursive: true });
+    }
+
+    // convert the page to pdf
+    const promiseBuffer = await page.pdf({
+        path: path.resolve(outDir, fileName),
+        format: "a4",
+    });
+
+    // close the browser
+    await browser.close();
+
+    // return the buffer
+    return promiseBuffer;
+}
--- test/pdffromhtml.test.ts
+++ test/pdffromhtml.test.ts
@@ -5,20 +5,25 @@ Created: Thu Jan 27 2022 22:31:09 GMT+0530 (India Standard Time)
 Copyright (c) geekofia 2022 and beyond
 */
 
-import PdfFromHtml from "../src/pdffromhtml";
+import { pdfFromHtml } from "../src/pdffromhtml";
+import * as path from "path";
+import * as fs from "fs";
 
 /**
  * Basic tests for the PdfFromHtml class.
  */
 
-it("checks the instance of Function", () => {
-    expect(PdfFromHtml).toBeInstanceOf(Function);
+describe("Basic test:", () => {
+    it("checks the instance of pdfFromHtml", () => {
+        expect(pdfFromHtml).toBeInstanceOf(Function);
+    });
 });
 
-describe("PdfFromHtml.create()", () => {
-    const content = "<p>test</p>";
-    const fileName = "test.pdf";
-    const pdfFromHtmlCreate = PdfFromHtml(content, fileName);
+describe("Tests for pdfFromHtml():", () => {
+    const content = "<h1>This is a Heading!</h1>";
+    const outDir = "./test/out/v2";
+    const fileName = `pdf_${Date.now()}.pdf`;
+    const pdfFromHtmlCreate = pdfFromHtml(content, fileName, outDir);
 
     it("does not returns null", () => {
         expect(pdfFromHtmlCreate).not.toBeNull();
@@ -33,5 +38,9 @@ describe("PdfFromHtml.create()", () => {
         return pdfFromHtmlCreate.then(data =>
             expect(data).toBeInstanceOf(Buffer),
         );
+    });
+
+    it(`it creates ${fileName} at ${path.resolve(outDir, fileName)}`, () => {
+        expect(fs.existsSync(path.resolve(outDir, fileName))).toBeTruthy();
     });
 });
